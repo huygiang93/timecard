ef8ded019bb6d07f142bed088e607489cc4a674b
huygiang93 <huygiang1393@gmail.com>
2020-01-22 15:36:45 -0800

    Delete wv_terms.py
    unnecessary item

--- wv_terms.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-
-terms_page = webdriver.Firefox()
-terms_page.get("https://westvalley.fastime.com/Individual/TermsofUse.aspx?type=0")
-
-terms_page.find_element_by_id("btnAccept").click()
